f6764c8d8093214c7adfdfa6bdf9f7aa7f5621a2
kazim hussain <kazimhussain716@gmail.com>
2017-08-02 13:15:55 +0500

    Added Mongolab

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,8 @@
 var mongoose = require('mongoose');
 
 mongoose.Prmoise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
+var MONGODB_URI = 'mongodb://<todoapp>:<Kazim123456789>@ds129043.mlab.com:29043/todoapp2';
+mongoose.connect(MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
 
 module.exports = {
     mongoose
