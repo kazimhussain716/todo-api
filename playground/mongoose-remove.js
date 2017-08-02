ae63a9e8e86a0c07f486a55d213d84a3feb7222d
kazim hussain <kazimhussain716@gmail.com>
2017-08-02 16:13:58 +0500

    Add Delete todos By id route

--- playground/mongoose-remove.js
+++ playground/mongoose-remove.js
@@ -1,8 +1,16 @@
 const { mongoose } = require('./../server/db/mongoose');
 const { Todo } = require('./../server/models/todo');
 const { User } = require('./../server/models/user');
-const {ObjectID} = require('mongodb');
+const { ObjectID } = require('mongodb');
 
-Todo.remove({}).then((result) => {
-     console.log(result);
+// Todo.remove({}).then((result) => {
+//      console.log(result);
+// });
+
+// findOneAndRemove()
+Todo.findOneAndRemove({ _id: '5981afb816dca95d5b1469ae' }).then((todo) => {
+    console.log(todo);
+})
+Todo.findByIdAndRemove('5981afb816dca95d5b1469ae').then((todo) => {
+    console.log(todo);
 })
