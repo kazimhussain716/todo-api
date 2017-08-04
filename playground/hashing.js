eb23d58e95e64e89407c7a407012db7ba95def3a
kazim hussain <kazimhussain716@gmail.com>
2017-08-04 14:43:43 +0500

    Hash passwords before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,13 +1,27 @@
 const { SHA256 } = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const  bcrypt = require('bcryptjs');
 
-var data = { id: 10 };
+var password = '123abc';
+// bcrypt.genSalt(10,(err,salt) => {
+//     bcrypt.hash( password,salt,(err,hash) => {
+//       console.log(hash);
+//     });
+// });
 
-var token = jwt.sign(data,'123abc');
-console.log(token);
+var hashedPassword = '$2a$10$FOooPRGP8fwZD62x0JAfoeckgQ5vj9mOCDaID1aW3mrDc9XMwW1ga';
+bcrypt.compare(password,hashedPassword,(err,res) => {
+    console.log(res);
+});
 
-var decoded = jwt.verify(token,'123abc');
-console.log('decoded :',decoded);
+
+// var data = { id: 10 };
+
+// var token = jwt.sign(data,'123abc');
+// console.log(token);
+
+// var decoded = jwt.verify(token,'123abc');
+// console.log('decoded :',decoded);
 
 
 
