79b4afc70920bdd76dcb8d9b78c83ca0e63a2475
kazim hussain <kazimhussain716@gmail.com>
2017-08-04 21:32:13 +0500

    Add Delete /users/me/token

--- server/models/user.js
+++ server/models/user.js
@@ -67,6 +67,17 @@ UserSchema.statics.findByToken = function (token) {
   })
 }
 
+UserSchema.methods.removeToken = function(token) {
+  var user = this;
+  return user.update({
+    $pull : {
+      tokens : {
+        token : token
+      }
+    }
+  })
+}
+
 UserSchema.statics.findByCredentials = function (email, password) {
   var User = this;
   return User.findOne({ email })
